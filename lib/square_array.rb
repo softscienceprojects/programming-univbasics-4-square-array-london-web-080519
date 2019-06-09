af2b7172ac89f2a1b2aecfcbab74656823c2790f
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-09 11:35:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,16 +3,11 @@ def square_array(array)
   myArr = []
   counter = 0
 
-  while array[counter] < array.length do
+  while counter < array.length do
     y = array[counter] * array[counter]
     myArr.push(y)
     counter += 1
   end
 
-  # array.each do |x|
-  #   y = x*x
-  #   myArr.push(y)
-  # end
-  # myArr
   myArr
 end
